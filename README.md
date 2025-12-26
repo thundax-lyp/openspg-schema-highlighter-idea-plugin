d904dfb52475c25c0c941d2cfdc1823ae3c9f389
thundax-lyp <731378491@qq.com>
2025-12-26 18:29:21 +0800

    refactor

---
+++ README.md
@@ -0,0 +1,32 @@
+# OpenSPG Schema Idea Plugin
+
+An IDEA plugin for OpenSPG Schema Mark Language
+
+- [JETBRAINS Marketplace](https://plugins.jetbrains.com/plugin/26288-openspg-schema-mark-language-highlighter/)
+
+## Preview
+
+<div align="center">
+    <img src="./docs/resources/screenshot.png" width="1200" alt="screenshot">
+</div>
+
+## Features:
+
+- Code style settings
+- Color setting page
+- Commenter
+- Completion
+- Formatter
+- Line marker
+- Reference
+- Syntax highlighter
+- Structure view
+- Preview
+- Folding
+
+## TODO
+
+- Annotator
+- Quick fix
+- Spell checking
+- Find usages
---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.25"
+    id("org.jetbrains.intellij") version "1.17.4"
+}
+
+group = "org.openspg.idea"
+version = "0.0.16"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.alibaba:fastjson:2.0.57")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("src/main/gen")
+        }
+    }
+}
+
+
+// Configure Gradle IntelliJ Plugin
+// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    version.set("2023.1")
+    type.set("IC")
+
+    plugins.set(listOf("com.intellij.java"))
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("231")
+        untilBuild.set("252.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
