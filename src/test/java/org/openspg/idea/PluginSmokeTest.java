abd02fbac472f7d7dafd65a84d5a674d1b84d2ed
thundax-lyp <731378491@qq.com>
2026-01-12 16:29:13 +0800

    Feat(Test): add comments

--- src/test/java/org/openspg/idea/PluginSmokeTest.java
+++ src/test/java/org/openspg/idea/PluginSmokeTest.java
@@ -4,6 +4,11 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 
 public class PluginSmokeTest extends BasePlatformTestCase {
 
+    /**
+     * Scenario: minimal platform test run
+     * Focus: BasePlatformTestCase lifecycle is available
+     * Assert: simple assertions execute
+     */
     public void testPlatform() {
         System.out.println("=".repeat(40));
         assertTrue(true);
