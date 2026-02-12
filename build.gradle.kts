682c00d0898d25fee6e3da6a4f8f7e0576ca0608
thundax-lyp <731378491@qq.com>
2026-02-12 12:14:20 +0800

    Feat(Schema): generate grammar commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.grammarkit.tasks.GenerateLexerTask
+import org.jetbrains.grammarkit.tasks.GenerateParserTask
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
     id("org.jetbrains.intellij.platform") version "2.10.5"
+    id("org.jetbrains.grammarkit") version "2022.3.1"
     id("checkstyle")
 }
 
@@ -19,6 +23,9 @@ repositories {
 
 dependencies {
     implementation("com.alibaba:fastjson:2.0.57")
+    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
+    implementation("io.opentelemetry:opentelemetry-api:1.31.0")
+    implementation("io.opentelemetry:opentelemetry-context:1.31.0")
 
     intellijPlatform {
         intellijIdeaCommunity("2024.2")
@@ -29,6 +36,55 @@ dependencies {
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.opentest4j:opentest4j:1.3.0")
+}
+
+val grammarKitRuntime: Configuration? = configurations.detachedConfiguration(
+    dependencies.create("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7"),
+    dependencies.create("io.opentelemetry:opentelemetry-api:1.31.0"),
+    dependencies.create("io.opentelemetry:opentelemetry-context:1.31.0")
+)
+
+tasks {
+    register<GenerateParserTask>("generateSchemaParser") {
+        sourceFile.set(file("src/main/lexer/org/openspg/idea/schema/grammar/Schema.bnf"))
+        targetRoot.set("src/main/gen")
+        pathToParser.set("org/openspg/idea/schema/parser/_SchemaParser.java")
+        pathToPsiRoot.set("org/openspg/idea/schema/psi")
+        purgeOldFiles.set(true)
+    }
+
+    register<GenerateLexerTask>("generateSchemaLexer") {
+        sourceFile.set(file("src/main/lexer/org/openspg/idea/schema/lexer/Schema.flex"))
+        targetDir.set("src/main/gen/org/openspg/idea/schema/lexer")
+        targetClass.set("_SchemaLexer")
+        purgeOldFiles.set(true)
+    }
+
+    register<GenerateParserTask>("generateConceptRuleParser") {
+        sourceFile.set(file("src/main/lexer/org/openspg/idea/conceptRule/grammar/ConceptRule.bnf"))
+        targetRoot.set("src/main/gen")
+        pathToParser.set("org/openspg/idea/conceptRule/parser/ConceptRuleParser.java")
+        pathToPsiRoot.set("org/openspg/idea/conceptRule/psi")
+        purgeOldFiles.set(true)
+    }
+
+    register<GenerateLexerTask>("generateConceptRuleLexer") {
+        sourceFile.set(file("src/main/lexer/org/openspg/idea/conceptRule/lexer/ConceptRule.flex"))
+        targetDir.set("src/main/gen/org/openspg/idea/conceptRule/lexer")
+        targetClass.set("ConceptRuleLexer")
+        purgeOldFiles.set(true)
+    }
+
+    register("generateGrammarSources") {
+        group = "build"
+        description = "Generate parsers and lexers from .bnf and .flex files"
+        dependsOn(
+            "generateSchemaParser",
+            "generateSchemaLexer",
+            "generateConceptRuleParser",
+            "generateConceptRuleLexer"
+        )
+    }
 }
 
 checkstyle {
@@ -66,12 +122,20 @@ intellijPlatform {
 }
 
 tasks {
+    withType<GenerateParserTask>().configureEach {
+        classpath += grammarKitRuntime
+    }
+
+    withType<GenerateLexerTask>().configureEach {
+        classpath += grammarKitRuntime
+    }
+
     // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
     test {
