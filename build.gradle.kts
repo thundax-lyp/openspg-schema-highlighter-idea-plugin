5e4086c5c740f459f9c05f89a4cf7d89da96fbd4
thundax-lyp <731378491@qq.com>
2026-02-12 16:11:28 +0800

    Feat(Schema): remove grammar commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
-import org.jetbrains.grammarkit.tasks.GenerateLexerTask
-import org.jetbrains.grammarkit.tasks.GenerateParserTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
     id("org.jetbrains.intellij.platform") version "2.10.5"
-    id("org.jetbrains.grammarkit") version "2022.3.1"
+    id("org.jetbrains.grammarkit") version "2023.3.0.2"
     id("checkstyle")
 }
 
@@ -23,9 +21,6 @@ repositories {
 
 dependencies {
     implementation("com.alibaba:fastjson:2.0.57")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7")
-    implementation("io.opentelemetry:opentelemetry-api:1.31.0")
-    implementation("io.opentelemetry:opentelemetry-context:1.31.0")
 
     intellijPlatform {
         intellijIdeaCommunity("2024.2")
@@ -38,53 +33,9 @@ dependencies {
     testImplementation("org.opentest4j:opentest4j:1.3.0")
 }
 
-val grammarKitRuntime: Configuration? = configurations.detachedConfiguration(
-    dependencies.create("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.7"),
-    dependencies.create("io.opentelemetry:opentelemetry-api:1.31.0"),
-    dependencies.create("io.opentelemetry:opentelemetry-context:1.31.0")
-)
-
-tasks {
-    register<GenerateParserTask>("generateSchemaParser") {
-        sourceFile.set(file("src/main/lexer/org/openspg/idea/schema/grammar/Schema.bnf"))
-        targetRoot.set("src/main/gen")
-        pathToParser.set("org/openspg/idea/schema/parser/_SchemaParser.java")
-        pathToPsiRoot.set("org/openspg/idea/schema/psi")
-        purgeOldFiles.set(true)
-    }
-
-    register<GenerateLexerTask>("generateSchemaLexer") {
-        sourceFile.set(file("src/main/lexer/org/openspg/idea/schema/lexer/Schema.flex"))
-        targetDir.set("src/main/gen/org/openspg/idea/schema/lexer")
-        targetClass.set("_SchemaLexer")
-        purgeOldFiles.set(true)
-    }
-
-    register<GenerateParserTask>("generateConceptRuleParser") {
-        sourceFile.set(file("src/main/lexer/org/openspg/idea/conceptRule/grammar/ConceptRule.bnf"))
-        targetRoot.set("src/main/gen")
-        pathToParser.set("org/openspg/idea/conceptRule/parser/ConceptRuleParser.java")
-        pathToPsiRoot.set("org/openspg/idea/conceptRule/psi")
-        purgeOldFiles.set(true)
-    }
-
-    register<GenerateLexerTask>("generateConceptRuleLexer") {
-        sourceFile.set(file("src/main/lexer/org/openspg/idea/conceptRule/lexer/ConceptRule.flex"))
-        targetDir.set("src/main/gen/org/openspg/idea/conceptRule/lexer")
-        targetClass.set("ConceptRuleLexer")
-        purgeOldFiles.set(true)
-    }
-
-    register("generateGrammarSources") {
-        group = "build"
-        description = "Generate parsers and lexers from .bnf and .flex files"
-        dependsOn(
-            "generateSchemaParser",
-            "generateSchemaLexer",
-            "generateConceptRuleParser",
-            "generateConceptRuleLexer"
-        )
-    }
+grammarKit {
+    jflexRelease.set("1.9.2")
+    grammarKitRelease.set("2023.3")
 }
 
 checkstyle {
@@ -98,7 +49,18 @@ checkstyle {
 sourceSets {
     main {
         java {
-            srcDir("src/main/gen")
+            srcDirs("src/main/java", "src/main/gen")
+        }
+        resources {
+            srcDirs("src/main/resources")
+        }
+    }
+    test {
+        java {
+            srcDirs("src/test/java")
+        }
+        resources {
+            srcDirs("src/test/resources")
         }
     }
 }
@@ -122,14 +84,6 @@ intellijPlatform {
 }
 
 tasks {
-    withType<GenerateParserTask>().configureEach {
-        classpath = files(classpath, grammarKitRuntime)
-    }
-
-    withType<GenerateLexerTask>().configureEach {
-        classpath = files(classpath, grammarKitRuntime)
-    }
-
     // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
@@ -138,6 +92,7 @@ tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
     }
+
     test {
         useJUnit()
         testLogging {
